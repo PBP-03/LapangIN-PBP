0989b9fbb549eb5675c5625093f6d28df5eda954
Muhammad Fauzan <fauzannmuhh@gmail.com>
2025-10-21 21:32:47 +0700

    feat: components

--- lapangin/settings.py
+++ lapangin/settings.py
@@ -33,6 +33,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1","muhammad-fauzan44-lapangin.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://muhammad-fauzan44-lapangin.pbp.cs.ui.ac.id"
+]
+
 
 # Application definition
 
@@ -61,7 +65,7 @@ ROOT_URLCONF = 'lapangin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], # Tambahkan konten baris ini
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -139,7 +143,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
