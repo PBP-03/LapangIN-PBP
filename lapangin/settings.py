a213db7a1496f981935116e0e7de7dfe1fc03529
Muhammad Fauzan <fauzannmuhh@gmail.com>
2025-10-23 00:47:17 +0700

    feat: add seeder

--- lapangin/settings.py
+++ lapangin/settings.py
@@ -157,6 +157,10 @@ STATICFILES_DIRS = [
 # Direktori untuk mengumpulkan semua file static saat production
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # merujuk ke /staticfiles untuk collectstatic
 
+# Media files (User uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
