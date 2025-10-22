94bcc820035f6a7b37d25a2930d4aa0a874875cf
Muhammad Fauzan <fauzannmuhh@gmail.com>
2025-10-22 14:04:34 +0700

    feat: model

--- lapangin/settings.py
+++ lapangin/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'backend',
 ]
 
 MIDDLEWARE = [
@@ -156,3 +157,6 @@ else:
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom User Model
+AUTH_USER_MODEL = 'backend.User'
